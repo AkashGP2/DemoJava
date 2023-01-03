20290fca0a9265ab860c0f672880d6759cd28b76
AkashGP2 <vakash@deloitte.com>
2023-01-03 18:00:18 +0530

    second code added

---
+++ Second.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Second {
+ public static void main(String args[]){
+    System.out.println("Give 2 numbers a,b for addition");
+    Scanner t = new Scanner(System.in);
+    int a,b;
+    a = t.nextInt();
+    b = t.nextInt();
+    int c = a+b;
+    System.out.println("Sum is"+c);
+
+ }    
+}
