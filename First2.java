b7f9f23de4881d77a220904da65ce66e2832bc40
AkashGP2 <121854741+AkashGP2@users.noreply.github.com>
2023-01-19 16:52:43 +0530

    Delete First2.java

--- First2.java
+++
@@ -1,6 +0,0 @@
-Class First2{
-    public static void main(String args[]){
-        System.out.println("Earlier code has String in flower brackets");
-        System.out.println("Hello git first branch");
-    }
-}
